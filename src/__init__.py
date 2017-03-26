555927fd687313c4aa49635a937fb91a6349a6fa
Dustin Harshman <dustin.harshman@gmail.com>
2017-03-26 10:51:18 -0700

    __init__.py added

---
+++ src/__init__.py
@@ -0,0 +1,41 @@
+'''
+Created on Jan 23, 2017
+
+@author: dustin@animus.tech
+
+an·i·mus (noun)
+    
+    The animation of forces driving us to bring the unmanifested into this world.
+    From soul, heart, and mind, arise imagination, memory, and purpose.
+'''
+
+class Soul(Entropic):
+    ''' Immortal part of being
+    '''
+
+class Heart(Affectionate):
+    ''' Innermost part of being
+    '''
+
+class Mind(Conscious):
+    ''' Awareness of the world
+    '''
+
+class Imagination(Brave):
+    ''' Conception of the abstract
+    '''
+
+class Memory(Loving):
+    ''' Storage and recall of information
+    '''
+
+class Purpose(Ultimate):
+    ''' Reason for existence
+    '''
+
+class Human(Soul, Heart, Mind, Imagination, Memory, Purpose):
+    ''' Realizer of the unmanifested
+    '''
+
+
+# EOF
